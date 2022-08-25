4946223e9d304f9fa14165ec32255d68b0281764
DeveloperStack2022 <jlzyjose@gmail.com>
2022-08-25 02:33:39 -0500

    commit

--- src/routes/admin.py
+++ src/routes/admin.py
@@ -1,3 +1,4 @@
+
 from flask import Blueprint, request,jsonify
 import bcrypt
 from functions_jwt import validate_token
@@ -57,10 +58,22 @@ def getUsers():
     return json.dumps(users, default=json_util.default)
     # return {"Datos":users}
 
+@user_admin.route('/getUsersEstudiantes')
+def getUserStudent():
+    rol = mydb.db.role.find_one({"name_role":"estudiante"})
+    print(rol)
+    estudiantes = mydb.users.find({'rol_id':rol['_id']})
+    students = [ dict(row) for row in estudiantes ]
+    return json.dumps(students, default=json_util.default)
+  
 
-@user_admin.route('/getDocente',methods=["GET"])
-def getDocente():
-    return {"Ok":True}
+#Docente 
+@user_admin.route('/getUsersDocentes')
+def getDocentes():
+    docente = mydb.db.role.find_one({"name_role":"docente"})
+    docentes = mydb.users.find({"rol_id":docente['_id']})
+    docenteList = [ dict(row) for row in docentes ]
+    return json.dumps(docenteList, default=json_util.default)
 
 @user_admin.route('/deleteUser',methods=["DELETE"])
 def deleteUser():
@@ -105,7 +118,7 @@ def CreateMateria():
     if viernes:
         datos.append('Viernes')
     
-    mydb.materia.insert_one({"descripcion":descripcion,"hora_inicio":horaInicio,"hora_final":horaFinal,"dias":datos})
+    mydb.materia.insert_one({"descripcion":descripcion,"hora_inicio":horaInicio,"hora_final":horaFinal,"dias":datos,"aula":aula})
 
     response = jsonify({
         "ok":"ok",
@@ -173,18 +186,23 @@ def createCicloAcademico():
     json = request.json
     descripcion = json['descripcion']
     orden = json['orden']
- 
-
-    mydb.ciclo_academico.insert_one({"descripcion":descripcion,"orden":orden,"estado":False})
+    cicloAcademico = mydb.ciclo_academico.find();
+    cicloAca = [dict(row) for row in cicloAcademico]
+    print(cicloAca)
+    if not cicloAca: 
+        mydb.ciclo_academico.insert_one({"descripcion":descripcion,"orden":orden,"estado":True})
+    else:
+        mydb.ciclo_academico.insert_one({"descripcion":descripcion,"orden":orden,"estado":False})
 
     return {
         "ok":True
     }
-@user_admin.route('/getCicloAcademico',methods=["GET"])
+
+@user_admin.route('/getCicloAcademicos',methods=["GET"])
 def getCicloAcademico():
     ciclo_academico = mydb.ciclo_academico.find()
     ciclos = [ dict(row) for row in ciclo_academico ]
-    return json.dumps(ciclos,default=json.util.default)
+    return json.dumps(ciclos,default=json_util.default)
 
 @user_admin.route('/updateCicloAcademico/<id>',methods=["PUT"])
 def updateCicloAcademico(id):
@@ -200,14 +218,32 @@ def updateCicloAcademico(id):
     }
 @user_admin.route('/updateEstadoClicloAcademico/<id>',methods=["PUT"])
 def updateStateCicloAcademico(id):
-
     _id  = id
-    mydb.ciclo_academico.update_one({"_id":ObjectId(_id['$oid']) if "$oid" in _id else ObjectId(_id)}, {"$set":{"estado":True}})
+    idQuery = mydb.ciclo_academico.find_one({"_id":ObjectId(_id)})
+    if idQuery['estado']: 
+        mydb.ciclo_academico.update_one({"_id":ObjectId(_id['$oid']) if "$oid" in _id else ObjectId(_id)}, {"$set":{"estado":False}})
+        # querySizes = mydb.ciclo_academico.find({"estado":True})
+    else:
+        mydb.ciclo_academico.update_one({"_id":ObjectId(_id['$oid']) if "$oid" in _id else ObjectId(_id)}, {"$set":{"estado":True}})
+
+        # --------------------------- Reutilizar -------------------------------- # 
+
+        # if len(list(querySizes)) > 0 : 
+        #     # update many
+        #     # updateM = mydb.ciclo_academico.update_many({},{})
+        #     print(dict(querySizes))
+        #     for row in list(querySizes) : 
+        #         print('Row' + row["_id"])
+            # mydb.ciclo_academico.update_one({"_id":ObjectId(_id['$oid']) if "$oid" in _id else ObjectId(_id)}, {"$set":{"estado":True}})
+            
+        # mydb.ciclo_academico.update_one({"_id":ObjectId(_id['$oid']) if "$oid" in _id else ObjectId(_id)}, {"$set":{"estado":True}})
+    # else:
+
+
 
     return {
         "message":"Activate successfully"
     }
-
 
 #
 @user_admin.route('/createNota',methods=["POST"])
@@ -235,5 +271,74 @@ def AssignParalelo():
     docente = json['id_docente']
     materia = json['id_materia']
     paralelo = json['paralelo']
+    users = json['user']
     
-    # mydb.AssingParalelo.insert_one({})
+    mydb.AssingParalelo.insert_one({"id_ciclo":id_ciclo,"id_docente":docente,"id_materia":materia,"paralelo":paralelo,"students":users})
+
+    response = jsonify({
+        "ok":"ok",
+    })
+    response.status = 201;
+    return response;
+
+
+@user_admin.route('/getParalelos')
+def getParalelos():
+
+    paralelos = mydb.AssingParalelo.find()
+
+    # paralelosDic = [ dict(row) for row in paralelos ]
+    datosArray = []
+    datosUser = []
+    for row in paralelos :
+        # print("\n",dict(row))
+        valores  = dict(row)
+        _id = valores['_id']
+        id_ciclo = valores['id_ciclo']
+        id_materia = valores['id_materia']
+        id_docente = valores['id_docente']
+        studentsId = valores['students']
+
+        # Obtenemos el Documente del  ciclo Academico  (Query for Id )
+        cicloAcademico = mydb.ciclo_academico.find_one({"_id":ObjectId(id_ciclo)})
+        # print(dict(cicloAcademico))
+        descripcionDelCiclo  = cicloAcademico['descripcion']
+      
+        
+        #Obtenemos el Documente de la Materia  (Query for Id )
+        materia = mydb.materia.find_one({"_id":ObjectId(id_materia)})
+        # print(materia)
+        
+    
+        #Obtenemos el Documente de la Materia  (Query for Id )
+        docenteDocument = mydb.users.find_one({"_id":id_docente})
+        # print(docenteDocument)
+        
+        # print(studentsId)
+        count = 0;
+        for userRow in studentsId:
+            user = mydb.users.find_one({"_id":userRow})
+            # print("\n",dict(user))
+            count += 1
+            datosUser.append({"usuario":user['nombre']})
+            # datosArray.append({"usuario":user['nombre']})
+
+
+        datosArray.append({"_id":_id,"descripcion_ciclo":descripcionDelCiclo,"descripcion_materia":materia['descripcion'],"nombre_docente":docenteDocument['nombre']})
+        print(count)
+        
+        
+    # print(datosArray)
+ 
+    # return json.dumps(paralelosDic,default=json_util.default)
+    return json.dumps(datosArray,default=json_util.default)
+
+
+@user_admin.route('/deleteParalelo/<id>',methods=["DELETE"])
+def deleteParalelo(id):
+    _id = id
+    mydb.AssingParalelo.delete_one({"_id":ObjectId(_id)})
+
+    return {
+        "ok":"ok"
+    }
